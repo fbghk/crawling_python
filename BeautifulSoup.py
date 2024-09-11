e07e05ccfbc0ca22f67aa5e139ba95d64bae2b43
fbghk <im706374@gmail.com>
2024-09-11 17:24:51 +0900

    chapter4 | 파이썬으로 페이지네이션 처리하기

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,23 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+import requests
+from bs4 import BeautifulSoup
 
-# ChromeDriver의 경로를 지정하고 Chrome 옵션을 설정
-chrome_driver_path = 'C:\\Users\\Administrator\\Downloads\\chromedriver-win64\\chromedriver.exe'  # 다운로드한 크롬드라이버 경로로 수정
-service = Service(chrome_driver_path)
-options = Options()
-options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행
+# 기본 URL 설정 (page 파라미터 제외)
+base_url = 'https://statiz.sporki.com/schedule/?m=gamelogs&s_no='
 
-# 브라우저 열기
-driver = webdriver.Chrome(service=service, options=options)
+# 페이지 범위 설정 (1~5페이지까지 크롤링)
+for page_num in range(20241457, 20241459):
+    # 각 페이지의 URL 생성
+    url = base_url + str(page_num)
+    
+    # 웹사이트에 요청을 보내고 응답을 받음
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# 웹사이트 열기
-driver.get('https://statiz.sporki.com/schedule/?m=gamelogs&s_no=20241459')
+    # 원하는 데이터를 추출 (예: <div class="table_type03">의 내용)
+    items = soup.find_all('div', class_='table_type03')
+    
+    # 각 페이지에서 수집한 데이터 출력
+    for item in items:
+        print(item.text)
 
-# 원하는 데이터 가져오기 (예: <div class="box_head">) <div class="box_cont">
-box_cont = driver.find_element(By.CLASS_NAME, 'box_cont')
-print(box_cont.text)
-
-# 브라우저 닫기
-driver.quit()
+    print(f'Page {page_num} done!')
