db609d9622724677f82042566b4b0b86153a698e
fbghk <im706374@gmail.com>
2024-09-11 16:00:18 +0900

    chapter3 | 동적 콘텐츠 대응하기 | Selenium 기본 코드

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,13 +1,23 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-# 웹사이트에 요청을 보내고 응답을 받는다
-url = 'https://statiz.sporki.com/schedule/?m=gamelogs&s_no=20241459'
-response = requests.get(url)
+# ChromeDriver의 경로를 지정하고 Chrome 옵션을 설정
+chrome_driver_path = 'C:\\Users\\Administrator\\Downloads\\chromedriver-win64\\chromedriver.exe'  # 다운로드한 크롬드라이버 경로로 수정
+service = Service(chrome_driver_path)
+options = Options()
+options.add_argument("--headless")  # 브라우저 창을 띄우지 않고 실행
 
-# BeautifulSoup으로 HTML을 파싱한다
-soup = BeautifulSoup(response.text, 'html.parser')
+# 브라우저 열기
+driver = webdriver.Chrome(service=service, options=options)
 
-# 원하는 데이터를 추출한다 (예: <h1> 태그의 내용)
-box_head_text = soup.find('div', class_='box_head').text
-print(box_head_text)
+# 웹사이트 열기
+driver.get('https://statiz.sporki.com/schedule/?m=gamelogs&s_no=20241459')
+
+# 원하는 데이터 가져오기 (예: <div class="box_head">) <div class="box_cont">
+box_cont = driver.find_element(By.CLASS_NAME, 'box_cont')
+print(box_cont.text)
+
+# 브라우저 닫기
+driver.quit()
